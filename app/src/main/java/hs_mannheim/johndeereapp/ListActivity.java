7d4cc381a2bf05a888b7afdba94d7b6cba1feecf
Horst Schneider <h.schneider@hs-mannheim.de>
2015-04-10 13:50:35 +0200

    Added simple list adapter that populates a list with values from the database.

--- app/src/main/java/hs_mannheim/johndeereapp/ListActivity.java
+++ app/src/main/java/hs_mannheim/johndeereapp/ListActivity.java
@@ -5,14 +5,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.Item;
+
+import java.util.ArrayList;
 
 
 public class ListActivity extends ActionBarActivity {
@@ -20,6 +28,7 @@ public class ListActivity extends ActionBarActivity {
     private static final int ITEM_LOGOUT = 1;
     private ProgressBar mProgressBar;
     private Button mSyncButton;
+    private ListView mListView;
     private boolean isSyncing;
 
     @Override
@@ -29,6 +38,8 @@ public class ListActivity extends ActionBarActivity {
 
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mSyncButton = (Button) findViewById(R.id.btn_sync);
+        mListView = (ListView) findViewById(R.id.lv_items);
+
         isSyncing = false;
 
         IntentFilter filter = new IntentFilter();
@@ -40,6 +51,8 @@ public class ListActivity extends ActionBarActivity {
                 ListActivity.this.onSynced();
             }
         }, filter);
+
+
     }
 
     @Override
@@ -91,8 +104,87 @@ public class ListActivity extends ActionBarActivity {
         Cursor itemsCursor = helper.getReadableDatabase().query("Items", new String[]{"ID", "Title", "Description"}, null, null, null, null, null);
 
         itemsCursor.moveToFirst();
-        do {
-            Log.d("jd", "Item: " + itemsCursor.getString(0));
-        } while(itemsCursor.moveToNext());
+
+        ArrayList items = new ArrayList();
+
+        itemsCursor.moveToFirst();
+        while (!itemsCursor.isAfterLast()) {
+            Item item = new Item(itemsCursor.getInt(0), itemsCursor.getString(1), itemsCursor.getString(2));
+            items.add(item);
+            itemsCursor.moveToNext();
+        }
+
+        mListView.setAdapter(new ItemAdapter(items));
+    }
+
+    public class ItemAdapter implements ListAdapter {
+
+        private ArrayList<Item> mItems;
+
+        public ItemAdapter(ArrayList<Item> items) {
+            mItems = items;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return true;
+        }
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv = new TextView(ListActivity.this);
+            tv.setText(mItems.get(position).getTitle());
+            return tv;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 1;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return mItems.size() <= 0;
+        }
     }
 }
