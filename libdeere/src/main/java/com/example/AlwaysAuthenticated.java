7dae0c66057f03659eca854a04328c5c60123b4f
Horst Schneider <h.schneider@hs-mannheim.de>
2015-04-10 16:08:28 +0200

    Little refactoring.

--- libdeere/src/main/java/com/example/AlwaysAuthenticated.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class AlwaysAuthenticated implements Authenticator {
-
-    @Override
-    public boolean authenticate(String username, String password) {
-        return true;
-    }
-}
